02dec233deb80c34b9141f3174b1b7219147a076
zhoffm <zhoffm@gmail.com>
2020-05-16 21:40:25 -0500

    added error handling and improved logging

--- shame_bot.py
+++ shame_bot.py
@@ -6,32 +6,35 @@ app = Flask(__name__)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+
 @app.route('/', methods=['POST'])
 def shame_bot():
     logging.info(request.args)
     logging.info(request.get_json())
     bot = telegram.Bot(token='1063153614:AAER4WaltVeBUrXZAZta07R4OLCh-ZwHaKY')
+    print(bot.getMyCommands)
+    print(bot.commands)
+    logging.info(bot.getMyCommands)
     logging.info(bot.commands)
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
-        if '/echo' in update.message.text:
-            bot.sendMessage(chat_id=chat_id, text=update.message.text)
-        elif '/help' in update.message.text:
-            bot.sendMessage(chat_id=chat_id, text='Fuck off!')
-        elif '/old' in update.message.text:
-            bot.sendMessage(chat_id=chat_id, text="haha Zach's old")
-        elif '/willsucks' in update.message.text:
-            bot.sendMessage(chat_id=chat_id, text="haha Will sucks!")
-        elif '/rollme' in update.message.text:
-            bot.sendDice(chat_id=chat_id)
-        elif '/peoplescourt' in update.message.text:
-            bot.sendPoll(chat_id=chat_id, question="People's court?", is_anonymous=False, open_period=600,
-                         options=["People's Court!", "Not People's Court!"])
-        elif '/' in update.message.text:
-            bot.sendMessage(chat_id=chat_id, text=f"{update.message.text} isn't a command, dumbass")
-        else:
-            return "Finished!", 200
+        if update.message.text:
+            if '/echo' in update.message.text:
+                bot.sendMessage(chat_id=chat_id, text=update.message.text)
+            elif '/help' in update.message.text:
+                bot.sendMessage(chat_id=chat_id, text='Fuck off!')
+            elif '/old' in update.message.text:
+                bot.sendMessage(chat_id=chat_id, text="haha Zach's old")
+            elif '/willsucks' in update.message.text:
+                bot.sendMessage(chat_id=chat_id, text="haha Will sucks!")
+            elif '/rollme' in update.message.text:
+                bot.sendDice(chat_id=chat_id)
+            elif '/peoplescourt' in update.message.text:
+                bot.sendPoll(chat_id=chat_id, question="People's court?", is_anonymous=False, open_period=600,
+                             options=["People's Court!", "Not People's Court!"])
+            elif '/' in update.message.text:
+                bot.sendMessage(chat_id=chat_id, text=f"{update.message.text} isn't a command, dumbass")
     return "Finished!", 200
 
 
