c11791f9e71b1457acca2500a770f6732a8b4952
zhoffm <zhoffm@gmail.com>
2022-01-18 22:17:14 -0600

    added a dank rat pic for my bro nathan and other chill upgrades

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
-gcloud builds submit --tag gcr.io/telegram-shame-bot-277421/telegram-shame-bot
-gcloud run deploy --image gcr.io/telegram-shame-bot-277421/telegram-shame-bot --platform managed
+GCP_PROJECT=telegram-shame-bot-277421
+APP_NAME=telegram-shame-bot
+gcloud config set project $GCP_PROJECT
+pack build telegram-shame-bot
+pack build --publish gcr.io/$GCP_PROJECT/$APP_NAME
+gcloud run deploy telegram-shame-bot --image gcr.io/$GCP_PROJECT/$APP_NAME --region us-central1 --platform managed --allow-unauthenticated
+gcloud run services update-traffic telegram-shame-bot --to-latest --platform managed --region us-central1
--- shame_bot.py
+++ shame_bot.py
@@ -1,96 +1,91 @@
 import logging
 from os import environ
-import telegram
-from telegram.botcommand import BotCommand
+
+from telegram import Update, Bot
+from telegram.ext import CallbackContext, Updater, CommandHandler, MessageHandler, Filters
+
 from flask import Flask, request
 app = Flask(__name__)
+
+TOKEN = '1063153614:AAER4WaltVeBUrXZAZta07R4OLCh-ZwHaKY'
+bot = Bot(token=TOKEN)
+updater = Updater(bot=bot, use_context=True)
+dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 shamebot_logs_url = 'https://console.cloud.google.com/run/detail/us-central1/telegram-shame-bot/logs?project=telegram-shame-bot-277421'
 
 
-@app.route('/', methods=['POST'])
+@app.post('/')
 def shame_bot():
-    bot = telegram.Bot(token='1063153614:AAER4WaltVeBUrXZAZta07R4OLCh-ZwHaKY')
     logging.info(request.get_json())
-    logging.info(bot.getMyCommands())
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        logging.info(update)
-        message = update.message
-        if message:
-            handle_commands(message, bot)
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
     return "Finished!", 200
 
 
-def handle_commands(message, bot):
-    chat_id = message.chat.id
-    from_user = message.from_user
-    mentions = message.parse_entities(['mention'])
-    mention_entity_list = list(mentions)
-    tag = None
-    if mention_entity_list:
-        tag = message.parse_entity(mention_entity_list[0])
+def do_echo(update: Update, context: CallbackContext):
+    entities = update.message.parse_entities(types=['bot_command'])
+    for entity, entity_text in entities.items():
+        stripped_text = update.message.text.lstrip(entity_text).strip()
+        context.bot.send_message(chat_id=update.effective_chat.id, text=stripped_text)
 
-    def do_echo():
-        bot.sendMessage(chat_id, text=message.text)
 
-    def do_help():
-        bot.sendMessage(chat_id, text='lol fuck off')
+def do_help(update: Update, context: CallbackContext):
+    context.bot.sendMessage(update.effective_chat.id, text='lol fuck off')
 
-    def do_old():
-        bot.sendMessage(chat_id, text="haha Zach's old")
 
-    def do_willsucks():
-        bot.sendMessage(chat_id, text="haha Will sucks")
+def do_old(update: Update, context: CallbackContext):
+    context.bot.sendMessage(update.effective_chat.id, text="haha Zach's old")
 
-    def do_rollme():
-        bot.sendDice(chat_id)
 
-    def do_peoplescourt():
-        bot.sendPoll(chat_id, question="People's court?", is_anonymous=False, open_period=600,
-                     options=["People's Court!", "Not People's Court!"])
+def do_willsucks(update: Update, context: CallbackContext):
+    context.bot.sendMessage(update.effective_chat.id, text="haha Will sucks")
 
-    def do_yipos():
-        if tag:
-            bot.sendMessage(chat_id, text=f"{tag}, you inflammatory piece of shit")
-        else:
-            bot.sendMessage(chat_id, text=f"you inflammatory piece of shit")
 
-    def handle_empty_command():
-        bot.sendMessage(chat_id, text=f"{message.text} isn't a command, dumbass!")
+def do_rollme(update: Update, context: CallbackContext):
+    context.bot.sendDice(update.effective_chat.id)
 
-    def debug():
 
-        if from_user.username == 'zhoffm':
-            logging.info(message)
-            logging.info(f'Chat ID: {chat_id}')
-            logging.info(f'From User: {from_user}')
-            logging.info(f'Mentions: {mentions}')
-            logging.debug(mention_entity_list)
-            logging.info(tag)
-            bot.sendMessage(chat_id, text=f"Papa! I've put my logs here: {shamebot_logs_url}")
-        else:
-            bot.sendMessage(chat_id, text=f"How dare you?! You're not my dad!")
+def do_peoplescourt(update: Update, context: CallbackContext):
+    context.bot.sendPoll(update.effective_chat.id, question="People's court?", is_anonymous=False, open_period=600,
+                    options=["People's Court!", "Not People's Court!"])
 
-    dispatch = {
-        'debug': debug,
-        'echo': do_echo,
-        'help': do_help,
-        'old': do_old,
-        'willsucks': do_willsucks,
-        'rollme': do_rollme,
-        'peoplescourt': do_peoplescourt,
-        'yipos': do_yipos,
-        '': handle_empty_command
-    }
 
-    for command in dispatch:
-        if f'/{command}' in message.text:
-            dispatch[command]()
-            return
+def do_yipos(update: Update, context: CallbackContext):
+    entities = update.message.parse_entities(types=['mention'])
+    if entities:
+        for entity, entity_text in entities.items():
+            context.bot.sendMessage(update.effective_chat.id, text=f"{entity_text}, you inflammatory piece of shit")
+    else:
+        context.bot.sendMessage(update.effective_chat.id, text=f"you inflammatory piece of shit")
+
+
+def do_hammyspammy(update: Update, context: CallbackContext):
+    hammy_spammy_photo_url = 'https://storage.googleapis.com/telegram-shame-bot-staticfiles/nathansdumbhammyspammy.jpg'
+    caption = "Here's your rat, you animal"
+    context.bot.sendPhoto(update.effective_chat.id, hammy_spammy_photo_url, caption=caption)
+
+
+def handle_empty_command(update: Update, context: CallbackContext):
+    entities = update.message.parse_entities(types=['bot_command'])
+    for entity, entity_text in entities.items():
+        context.bot.sendMessage(update.effective_chat.id, text=f"{entity_text} isn't a command, dumbass!")
+
+
+dispatcher.add_handler(CommandHandler('help', do_help))
+dispatcher.add_handler(CommandHandler('echo', do_echo))
+dispatcher.add_handler(CommandHandler('old', do_old))
+dispatcher.add_handler(CommandHandler('willsucks', do_willsucks))
+dispatcher.add_handler(CommandHandler('rollme', do_rollme))
+dispatcher.add_handler(CommandHandler('peoplescourt', do_peoplescourt))
+dispatcher.add_handler(CommandHandler('yipos', do_yipos))
+dispatcher.add_handler(CommandHandler('hammyspammy', do_hammyspammy))
+dispatcher.add_handler(CommandHandler('spammyhammy', do_hammyspammy))
+dispatcher.add_handler(MessageHandler(Filters.command, handle_empty_command))
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 8080)))
+    # updater.start_polling()
